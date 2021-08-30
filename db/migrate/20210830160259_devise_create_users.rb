e7b77dfb1d208643b306f2df787b1cfb2e0c851a
Ana Cury <apaulacury@gmail.com>
2021-08-30 14:50:38 -0300

    models-db

--- db/migrate/20210830160259_devise_create_users.rb
+++ db/migrate/20210830160259_devise_create_users.rb
@@ -6,6 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :first_name
+      t.string :last_name
+      t.string :cpf
+      t.string :phone_number
 
       ## Recoverable
       t.string   :reset_password_token

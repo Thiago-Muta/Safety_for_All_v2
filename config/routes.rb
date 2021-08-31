55db1ddb31cc64bcdc53e7bb34f4d86dc6e4e120
Thiago Muta <thimuta@hotmail.com>
2021-08-31 15:47:08 -0300

    rota v1

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-
+  resources :reports do
+    resources :reviews, only: [:new, :create]
+    patch 'close', to: 'reports#close'
+  end
+  resources :reviews, only: [:show, :destroy]
 
 end

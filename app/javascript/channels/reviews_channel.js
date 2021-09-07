c7c876d8352810d59fba5da8a4cf47c723c8bdc6
Thiago Muta <thimuta@hotmail.com>
2021-09-07 13:00:15 -0300

    tentou atualizar em tempo real a review no index

--- app/javascript/channels/reviews_channel.js
+++ app/javascript/channels/reviews_channel.js
@@ -8,10 +8,17 @@ const initReviewsCable = () => {
 
     consumer.subscriptions.create({ channel: "ReviewsChannel", id: id }, {
       received(data) {
+
+        const reportReviewCount = document.getElementById(`review-count-${data.report_id}`);
+        console.log(reportReviewCount)
+        if (reportReviewCount) { reportReviewCount.innerText = data.review_count };
+
         const noReviewsContainer = document.getElementById('no-reviews-container');
-        console.log(noReviewsContainer);
         if (noReviewsContainer) { noReviewsContainer.remove() };
+
         reviewsContainer.insertAdjacentHTML('beforeend', data.partial);
+
+
         if (data.review_count == 1) {
           const reviewsCounter = `<strong>Total reviews: <span id="review-count"> ${data.review_count}</span></strong>`
           reviewsContainer.insertAdjacentHTML('afterend', reviewsCounter);

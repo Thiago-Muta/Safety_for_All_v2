aec862ea2f3171bcd3f6e7023c198b53af4a5dd6
Tasso Rodrigues <tasso_henrique@hotmail.com>
2021-09-08 16:47:00 -0400

    show de reviews "pronto"

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,7 +6,7 @@ class ReviewsController < ApplicationController
 
   def new
     @report = Report.find_by(id: params[:report_id])
-    redirect_to reports_path, notice: 'Report not found!' if @report.nil?
+    redirect_to reports_path, notice: 'Denúncia não encontrada' if @report.nil?
     @review = Review.new
     authorize @review
   end
@@ -21,12 +21,12 @@ class ReviewsController < ApplicationController
       if @review.save
         ReviewsChannel.broadcast_to(@report, {report_id: @report.id, review_count: @report.reviews.length, partial: render_to_string(partial: 'review', locals: {review: @review})})
         #  ActionCable.server.broadcast(@report, render_to_string(partial: 'review', locals: { review: @review }))
-        redirect_to report_path(@report), notice: 'review was successfully created.'
+        redirect_to report_path(@report), notice: 'Comentário adicionado.'
       else
         render :new
       end
     else
-      redirect_to @report, alert: 'Report already Finished!'
+      redirect_to @report, alert: 'Denúncia finalizada'
     end
   end
 
